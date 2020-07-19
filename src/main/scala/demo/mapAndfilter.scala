a9cc89a734fd6f83584e5311f1d6c793e7be8187
abhisheklomshTD <abhishek.l@tatrasdata.com>
2020-07-19 22:45:46 +0530

    update

--- src/main/scala/demo/mapAndfilter.scala
+++ src/main/scala/demo/mapAndfilter.scala
@@ -1,11 +1,13 @@
 package demo
 
 object mapAndfilter {
-  val lst =  List(1,2,3,5,7,10,13);
+  val lst = List(1, 2, 3, 5, 7, 10, 13);
   val map = Map(1 -> "Tom", 2 -> "Max", 3 -> "John");
-  val opt : Option[Int] = Some(5);
+  val opt: Option[Int] = Some(5);
+
   //  val opt : Option[Int] = None;
-  def main(args: Array[String]){
-  println(lst.map(_ * 2));
+  def main(args: Array[String]) {
+    println(lst.map(_ * 2));
+    println(lst.map(x => x + 2));
   }
 }
