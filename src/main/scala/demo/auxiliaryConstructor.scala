0efcbebb0be4b765e5ffa33c138c53456d66b605
abhisheklomsh <abhisheklomsh@gmail.com>
2020-07-22 14:17:36 +0530

    update m

--- src/main/scala/demo/auxiliaryConstructor.scala
+++ src/main/scala/demo/auxiliaryConstructor.scala
@@ -4,23 +4,27 @@ package demo
 // var getter setter
 // val getter ----
 // default ----   ----
-class User(private var name: String, var age: Int){
-  def printName(){
-    println(name)
+class User2(private var name: String, var age: Int){
+def this(){
+  this("Tom",35);
+}
+  def this(name : String){
+    this(name, 22)
+  }
+  def this(age : Int){
+    this("Ram", age)
   }
 };
 
 object auxiliaryConstructor {
   def main(args: Array[String]): Unit = {
-    var user = new User("Max", 28);
-    user.printName();
-    //    println(user.name);
+    var user = new User2("Max", 28);
+    var user2 = new User2();
+    var user3 = new User2("Max");
     println(user.age);
-    //    user.name = "Tom";
-    user.age = 22;
-    //    println(user.name);
-    user.printName();
-    println(user.age);
+    println(user2.age);
+    println(user3.age);
+
   }
 }
 
